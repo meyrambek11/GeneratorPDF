7a5cba7dea3503143b2556fd0a2834f29a578e45
meyrambek11 <m_kadyrbekov@bk.ru>
2022-01-25 10:05:27 +0600

    deleted unneeded files

--- routers/to_pdf.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const toPdfController = require('../controllers/to_pdf');
-
-router.post('/create-pdf', toPdfController.createPdf);
-router.get('/fetch-pdf', toPdfController.getPdf);
-
-
-module.exports = router;
