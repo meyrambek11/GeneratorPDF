b0ce135d154235cadeac93dcc389e0e946fc5f8d
Yelaman Uisunbayev <yelaman.ui@gmail.com>
2022-01-25 11:12:20 +0600

    create template 70%

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -5,25 +5,37 @@ const htmlTemplate = require('../documents/htmlTemplate');
 class DocumentController{
     async createTemplate(req, res){
         const {userId, title, tags, html} = req.body
+        var tagList = [];
+        var templateId = 0;
         for (let i = 0; i < tags.length; i++){
             let name = tags[i].name;
             let value = tags[i].value;
             let type = tags[i].type;
-            pg_client.query(`INSERT INTO tags (name, value, type) VALUES ('${name}', '${value}', '${type})'`, (err, res) => {
-                if (err) res.json(err);
-                else res.json({msg: "success"});
-            });
+
+            pg_client.query(`INSERT INTO tags (name, value, type) VALUES ('${name}', '${value}', '${type}') RETURNING id`, (err, res) => { 
+                if (err) throw err;
+                else tagList.push(res.rows[0].id)
+             });
         }
-        
+
+        pg_client.query(`INSERT INTO templates (userId, title, html) VALUES (${userId}, '${title}', '${html}')RETURNING id`, (err, res) => { 
+            if (err) throw err;
+            else templateId = res.rows[0].id
+        });
+
+        for (let i = 0; i < tagList.length; i++){
+            pg_client.query(`INSERT INTO tags (name, value, type) VALUES ('${name}', '${value}', '${type}') RETURNING id`, (err, res) => { 
+                if (err) throw err;
+                else tagList.push(res.rows[0].id)
+             });
+        }
+        res.json({msg: "success"});
 
         // pg_client.query(`INSERT INTO template (id_comp, name) VALUES (${id}, '${name}');`, (err, res) => {
         //     if (err) throw err;
         //     else res.json({msg: "success"});
         // });
 
-        
-        // const newPerson = db.query('INSERT INTO person (name, surname) values ($1, $2) RETURNING *', [name,surname]);
-        // res.json((await newPerson).rows[0]);
     }
 
     async generateDocument(req, res){
