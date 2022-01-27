ee24dca2088d236b1814abbb9e611a4a9514e00e
meyrambek11 <m_kadyrbekov@bk.ru>
2022-01-27 16:16:05 +0600

    with delete template

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -1,4 +1,5 @@
 const pg_client = require('../db/connection');
+const format = require('pg-format');
 
 class DocumentController {
   async createTemplate(req, res) {
@@ -68,12 +69,16 @@ class DocumentController {
     });
   }
 
+
+
   // update template
   async updateTemplate(req, res) {
 
     res.status(200).json(result.rows)
 
   }
+
+
 
   //get all Templates
   async getTemplates(req, res) {
@@ -85,10 +90,13 @@ class DocumentController {
     });
   }
 
+
+
+
   // get tags by template id;
   async getTagsByTemplateID(req, res) {
-    const id = req.params.id;
-    const tags_id = (await pg_client.query('SELECT tag_id from template_tags_relationship where template_id = $1', [id])).rows;
+    const TemplateId = req.params.id;
+    const tags_id = (await pg_client.query('SELECT tag_id from template_tags_relationship where template_id = $1', [TemplateId])).rows;
     if (!tags_id) {
       res.status(400).json({
         success: false
@@ -109,6 +117,8 @@ class DocumentController {
   }
 
 
+
+
   //get template by id
   async getTemplateByID(req, res) {
     const id = req.params.id;
@@ -123,6 +133,64 @@ class DocumentController {
   //delete template
   async deleteTemplateByID(req, res) {
     const id = req.params.id;
+    const TemplateTagsID = [];
+    const tags_id = (await pg_client.query('SELECT tag_id from template_tags_relationship where template_id = $1', [id])).rows;
+    if (!tags_id) {
+      res.status(400).json({
+        success: false, msg: 'tags does not select from template_tags_relationship by templateID'
+      })
+    }
+    for(let i=0;i<tags_id.length;i++){
+      TemplateTagsID.push(tags_id[i].tag_id)
+    };
+    console.log("Template tags: " + TemplateTagsID);
+    
+    //Delete relationship by template ID
+    const isDeleteRelation = (await pg_client.query('DELETE from template_tags_relationship where template_id = $1', [id]));
+    if (!isDeleteRelation) {
+      res.status(400).json({
+        success: false, msg: 'Relation do not deleted'
+      })
+    }
+
+    const allRelationshipTagsID = [];   
+
+    const alltags = (await pg_client.query('SELECT tag_id from template_tags_relationship')).rows;
+    if (!alltags) {
+      res.status(400).json({
+        success: false, msg: 'All tags does not select from template_tags_relationship'
+      })
+    }
+    for(let i=0;i<alltags.length;i++){
+      allRelationshipTagsID.push(alltags[i].tag_id)
+    };
+    console.log("After deleting relationship tags " + allRelationshipTagsID);
+
+    // Filter tag names from template array, get those tags that doesn't exists in database
+    const DelatingTagsID = TemplateTagsID.filter(tag => !allRelationshipTagsID.includes(tag));
+    console.log("Deleting tags: " + DelatingTagsID);
+
+    //Delete template by ID
+    const isDeleteTemplate = (await pg_client.query('DELETE from templates where id = $1', [id]));
+    if (!isDeleteTemplate) {
+      res.status(400).json({
+        success: false, msg: 'Template do not delete from templates'
+      })
+    }
+
+    //Delete filtering tags
+    for(let i=0;i<DelatingTagsID.length;i++){
+      const isDeleteTag = (await pg_client.query('DELETE from tags where id = $1', [DelatingTagsID[i]]));
+      if (!isDeleteTag) {
+        res.status(400).json({
+          success: false, msg: 'Tag do not delete from tags'
+        })
+      }
+    }
+
+    res.status(200).json({succsess: true, msg: "Filtering TemplateTags, Template and Template from Relationship succsessfully deleted"});
+    
+
 
   }
 }
